11a3ee0da5f43fb4b9b91021132f4259856d0fa5
Seong Lee <selee@chapman.edu>
2017-12-06 17:23:34 +0000

    blas update

--- src/blas/boost_matrix.cc
+++ src/blas/boost_matrix.cc
@@ -1,5 +1,7 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/io.hpp>
+#include <iostream>
+#include <array>
 
 int main(int argc, char **argv) 
 {
@@ -27,17 +29,46 @@ int main(int argc, char **argv)
     }
   }
 
+  std::array<int,5> real = {-2, -1, 0, 1, 2};
+  std::array<int,5> imag = {-2, -1, 0, 1, 2};
+  int rs = real.size()-1;
+
+    
+  matrix<std::complex<long double> > cp(5, 5);
+    
+  for (unsigned r = 0; r < real.size(); r++) {
+    for (unsigned c = 0; c < imag.size(); c++) {
+      cp(r, c) = real[rs-r] + imag[c] * std::complex<long double>(0,1); 
+    }
+  }
+  
+    
+ 
+    
   // print to screen as demonstration
   cout << "m:" << endl;
   cout << m << endl;
   cout << endl << "n:" << endl;
   cout << n << endl;
   cout << endl << "o:" << endl;
-  cout << n << endl;
+  cout << o << endl;
   cout << endl << "m + n:" << endl;
   cout << m + n << endl;
   cout << endl << "m * n:" << endl;
   cout << prod(m, n) << endl;
   cout << endl << "n * n - o:" << endl;
   cout << prod(n, n) - o << endl;
+  cout << endl;
+    
+  cout << "complex plane:" << endl;
+    for(unsigned i=0;i<cp.size1();++i)
+    {
+        cout<<"| ";
+        for (unsigned j=0;j<cp.size2();++j)
+        {
+            cout<<cp(i,j)<<" | ";
+        }
+        cout << endl;
+    }
+
 }
